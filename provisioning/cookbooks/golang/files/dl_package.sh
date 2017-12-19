efa9dc09ce7a4bb411ac2afb604070f47d48cd06
saito <saitou@den3svc.jp>
2017-12-19 17:48:18 +0900

    プロビジョニング：golang修正5

---
+++ provisioning/cookbooks/golang/files/dl_package.sh
@@ -0,0 +1,6 @@
+export GOPATH=/home/iot/.go
+go get github.com/labstack/echo
+go get github.com/dgrijalva/jwt-go
+go get github.com/go-sql-driver/mysql
+go get github.com/gocraft/dbr
+go get github.com/pkg/errors
--- provisioning/cookbooks/golang/recipe.rb
+++ provisioning/cookbooks/golang/recipe.rb
@@ -7,25 +7,12 @@ remote_file ".bash_profile" do
   source "./files/.bash_profile"
 end
 
-# 環境変数セット
-execute ".bash_profile" do
-  command ".bash_profile"
+# ファイル転送
+remote_file "dl_package.sh" do
+  mode "775"
+  source "dl_package.sh"
 end
 
-# golangパッケージのダウンロード
-execute "go get github.com/labstack/echo" do
-  command "go get github.com/labstack/echo"
+execute ".dl_package.sh" do
+  command ".dl_package.sh"
 end
-execute "go get github.com/dgrijalva/jwt-go" do
-  command "go get github.com/dgrijalva/jwt-go"
-end
-execute "go get github.com/go-sql-driver/mysql" do
-  command "go get github.com/go-sql-driver/mysql"
-end
-execute "go get github.com/gocraft/dbr" do
-  command "go get github.com/gocraft/dbr"
-end
-execute "go get github.com/pkg/errors" do
-  command "go get github.com/pkg/errors"
-end
-
