3616fc2498b210bc45947e9f336f207c59d6de57
saito <saitou@den3svc.jp>
2017-12-21 14:11:12 +0900

    プロビジョニング：レシピ修正 パッケージ：更新

--- provisioning/cookbooks/nginx/recipe.rb
+++ provisioning/cookbooks/nginx/recipe.rb
@@ -5,5 +5,10 @@ package "nginx"
 ## 設定ファイルの書き換え
 # rootパス
 execute "configuration nginx" do
-  command "sed -i 's/[^#].*root.*$/\\troot \\/home\\/iot\\/sensor-management-web\\/dist;/' /etc/nginx/sites-available/default"
+  command "sed -i 's/^\\t.*root.*$/\\troot \\/home\\/iot\\/sensor-management-web\\/dist;/' /etc/nginx/sites-available/default"
+end
+
+# 再起動
+execute "Service restart nginx" do
+  command "sudo systemctl restart nginx.service"
 end
