7fc5380228cb5c2a915a127e6d30309ca1ab7a3c
saito <saitou@den3svc.jp>
2017-12-21 10:43:40 +0900

    プロビジョニング：MariaDB修正2

--- provisioning/cookbooks/MariaDB/recipe.rb
+++ provisioning/cookbooks/MariaDB/recipe.rb
@@ -3,11 +3,16 @@
 package "mariadb-server"
 
 # ファイル転送
-remote_file "~/create_user.sql" do
+remote_file "create_user.sql" do
   source "./files/create_user.sql"
 end
 
 # ユーザー作成
 execute "mysql -u root --default-character-set=utf8 < create_user.sql" do
   command "mysql -u root --default-character-set=utf8 < create_user.sql"
+end
+
+# 全てのIPアドレスからの接続を許す。開発マシン用の設定。本番では使わない。
+execute "sed -i 's/^\(bind-address\)/#\1/' /etc/mysql/mariadb.conf.d/50-server.cnf" do
+  command "sed -i 's/^\(bind-address\)/#\1/' /etc/mysql/mariadb.conf.d/50-server.cnf"
 end
--- provisioning/roles/iot.rb
+++ provisioning/roles/iot.rb
@@ -1,10 +1,10 @@
 ### IoTサーバーの構築
 
 # 基本的な設定やパッケージのインストール
-include_recipe "./cookbooks/base/recipe.rb"
+include_recipe "../cookbooks/base/recipe.rb"
 
 # golangのインストール
-include_recipe "./cookbooks/golang/recipe.rb"
+include_recipe "../cookbooks/golang/recipe.rb"
 
 # MariaDBのインストール
-include_recipe "./cookbooks/MariaDB/recipe.rb"
+include_recipe "../cookbooks/MariaDB/recipe.rb"
