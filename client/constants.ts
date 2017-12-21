348884ccc82fdcfa290faa0f83346ddb24810d99
saito <saitou@den3svc.jp>
2017-12-21 17:29:18 +0900

    ビルドコマンド修正

--- client/constants.ts
+++ client/constants.ts
@@ -1,4 +1,3 @@
 export namespace constants{
-    // export const PATH = "http://localhost:5555/"
-    export const PATH = "http://35.187.149.156:5555/"
+    export const PATH = "http://localhost:5555/"
 }
