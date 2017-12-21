ac8ee67b2a9fe708023791a6d3d2e29ca93d5f57
saito <saitou@den3svc.jp>
2017-12-21 12:03:47 +0900

    プロビジョニング：レシピ修正

--- provisioning/cookbooks/mariadb/recipe.rb
+++ provisioning/cookbooks/mariadb/recipe.rb
@@ -8,11 +8,11 @@ remote_file "create_user.sql" do
 end
 
 # ユーザー作成
-execute "mysql -u root --default-character-set=utf8 < create_user.sql" do
+execute "create user" do
   command "mysql -u root --default-character-set=utf8 < create_user.sql"
 end
 
 # 全てのIPアドレスからの接続を許す。開発マシン用の設定。本番では使わない。
-execute "sed -i 's/^\(bind-address\)/#\1/' /etc/mysql/mariadb.conf.d/50-server.cnf" do
+execute "configuration mariadb" do
   command "sed -i 's/^\(bind-address\)/#\1/' /etc/mysql/mariadb.conf.d/50-server.cnf"
 end
