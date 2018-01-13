9a99ff1b33ed7baa4a6452cc006f7752ab325324
masataka_nakajima <nakajima@inasys.jp>
2018-01-13 13:26:55 +0900

    setport.go

--- server/setport/setport.go
+++ server/setport/setport.go
@@ -35,9 +35,10 @@ func SetPort(c echo.Context) error {
 	//接続ポートテーブルのupdateSQLを発行する
 	//attrsMap := map[string]interface{}{"value": port.Value}//おいおい引数の設定値で更新する
 	//attrsMap := map[string]interface{}{"value": "on"}
+	// Where("serial_no = ?", port.Serial_no).
 	sess.Update("t_port").
 		Set("value", "on").
-		Where("serial_no = ?", port.Serial_no).
+		Where("port_no = 1").
 		Exec()
 	return c.JSON(http.StatusOK , "あっぷでーと")
 }
