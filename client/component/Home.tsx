3e5a2dddcf3277813c854e87fc99875545ce69ef
masataka_nakajima <nakajima@inasys.jp>
2018-02-07 17:13:19 +0900

    デザイン修正

--- client/component/Home.tsx
+++ client/component/Home.tsx
@@ -5,10 +5,12 @@ import { Field, reduxForm } from 'redux-form'
 const HomeForm = props => {
   return (
     <form>
+    <h1>タイトル</h1>
+
       <h3>電磁弁の操作</h3>
       <div>
-        <button type="button" onClick={() => props.actions.setPort(1, 'on')}>開ける</button>
-        <button type="button" onClick={() => props.actions.setPort(1, 'off')}>閉める</button>
+        <button type="button" onClick={() => props.actions.setPort(1, 'on')}>　On　</button>
+        <button type="button" onClick={() => props.actions.setPort(1, 'off')}>　Off　</button>
       </div>
 
       <h3>ポート2の操作</h3>
